548ce74d82a5ed4d80342204ea247de434ce3eaf
TheBard523 <72112073+TheBard523@users.noreply.github.com>
2020-10-05 11:30:19 +1100

    Added Initial Project
    Added The BookChase project to repository.

---
+++ The BookChase/Assets/Malbers Animations/Common/Scripts/Scriptables/RunTime Collections/RuntimeCollection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MalbersAnimations.Scriptables
+{
+    public abstract class RuntimeCollection<T> : ScriptableObject
+    {
+        protected List<T> items = new List<T>();
+
+        /// <summary>Ammount of object on the list</summary>
+        public int Count => items.Count;
+
+        /// <summary> Clears the list of objects </summary>
+        public void Clear() => items.Clear();
+
+        /// <summary>Gets an object on the list by an index </summary>
+        public T Item_Get(int index) => items[index];
+
+
+
+        /// <summary>Gets the first object of the list</summary>
+        public T Item_GetFirst() => items[0];
+
+       
+
+
+        /// <summary>Gets a rando first object of the list</summary>
+        public T Item_GetRandom()
+        {
+            if (items != null && items.Count > 0)
+            {
+                var rnd = new System.Random();
+                return items[rnd.Next(items.Count)];
+            }
+            return default;
+        }
+
+        public void Item_Add(T newItem)
+        {
+            if (!items.Contains(newItem))
+                items.Add(newItem);
+        }
+
+        public void Item_Remove(T newItem)
+        {
+            if (items.Contains(newItem))
+                items.Remove(newItem);
+        }
+    }
+}
---
+++ The BookChase/Assets/Malbers Animations/Common/Scripts/Scriptables/RunTime Collections/RuntimeGameObjects.cs
@@ -0,0 +1,12 @@
+﻿using MalbersAnimations.Events;
+using UnityEngine;
+
+namespace MalbersAnimations.Scriptables
+{
+    [CreateAssetMenu(menuName = "Malbers Animations/Scriptables/Collections/Game Object Collection", fileName = "New GameObject Collection")]
+    public class RuntimeGameObjects : RuntimeCollection<GameObject>
+    { 
+    }
+}
+
+ 
